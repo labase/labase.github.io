597a8d983f3054b673217f54a4757857c0210fb6
Carlo Emmanoel <carlo@ufrj.br>
2023-08-16 15:53:19 -0300

    revise for wsgi (8/16).

--- game/control/__init__.py
+++ game/control/__init__.py
@@ -1,27 +1,12 @@
-#! /usr/bin/env python
-# -*- coding: UTF8 -*-
-# Este arquivo é parte do programa SupyPlay
-# Copyright 2010–2022 Carlo Oliveira <carlo@nce.ufrj.br>,
-# `Labase <http://labase.activufrj.nce.ufrj.br>`__; `GPL <http://j.mp/GNU_GPL3>`__.
-#
-# SupyPlay é um software livre; você pode redistribuí-lo e/ou
-# modificá-lo dentro dos termos da Licença Pública Geral GNU como
-# publicada pela Fundação do Software Livre (FSF); na versão 2 da
-# Licença.
-#
-# Este programa é distribuído na esperança de que possa ser útil,
-# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
-# a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
-# Licença Pública Geral GNU para maiores detalhes.
-#
-# Você deve ter recebido uma cópia da Licença Pública Geral GNU
-# junto com este programa, se não, veja em <http://www.gnu.org/licenses/>
-"""Module module wide names.
+"""Module define app wide names.
 
 .. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
 
 Changelog
 ---------
+.. versionadded::    23.08
+        revise for wsgi (16).
+
 .. versionadded::    20.07
         add version file.
 
@@ -31,15 +16,20 @@ Changelog
 .. versionadded::    22.09
         Support import from other files.
 
+|   **Open Source Notification:** This file is part of open source program **Alite**
+|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <http://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 """
 import os
 # name and list your controllers here so their routes become accessible.
 # Enable debugging, which gives us tracebacks
 project_server = os.path.dirname(os.path.abspath(__file__))
 js_dir = os.path.join(project_server, '../view/stlib')
-css_dir = os.path.join(project_server, '../view/css')
-img_dir = os.path.join(project_server, '../view/image')
+css_dir = os.path.join(project_server, '../../css')
+img_dir = os.path.join(project_server, '../../image')
 tpl_dir = os.path.join(project_server, '../view/tpl')
+inf_dir = os.path.join(project_server, '../..')
 py_dir = os.path.join(project_server, '../view')
 model_dir = os.path.join(project_server, '../model')
 print("js_dir = ", js_dir)
--- game/control/static_controller.py
+++ game/control/static_controller.py
@@ -1,27 +1,12 @@
-#! /usr/bin/env python
-# -*- coding: UTF8 -*-
-# Este arquivo é parte do programa SupyPlay
-# Copyright 2010–2022 Carlo Oliveira <carlo@nce.ufrj.br>,
-# `Labase <http://labase.activufrj.nce.ufrj.br>`__; `GPL <http://j.mp/GNU_GPL3>`__.
-#
-# SupyPlay é um software livre; você pode redistribuí-lo e/ou
-# modificá-lo dentro dos termos da Licença Pública Geral GNU como
-# publicada pela Fundação do Software Livre (FSF); na versão 2 da
-# Licença.
-#
-# Este programa é distribuído na esperança de que possa ser útil,
-# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
-# a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
-# Licença Pública Geral GNU para maiores detalhes.
-#
-# Você deve ter recebido uma cópia da Licença Pública Geral GNU
-# junto com este programa, se não, veja em <http://www.gnu.org/licenses/>
 """Retrieves static files.
 
 .. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
 
 Changelog
 ---------
+.. versionadded::    23.08
+        revise for wsgi (16).
+
 .. versionadded::    20.07
         add version file.
 
@@ -30,6 +15,11 @@ Changelog
 
 .. versionadded::    22.09
         Support import from other files.
+
+|   **Open Source Notification:** This file is part of open source program **Alite**
+|   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <http://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 
 """
 import bottle
@@ -71,8 +61,10 @@ def ajs(filepath):
 
 
 # Static Routes
+# @get("/css/<filepath:re:.*\.(js|css|map)>")
 @get("/css/<filepath:re:.*\.(js|css|map)>")
 def ajs(filepath):
+    print("/css/<filepath:re:.*\.(js|css|map)>", css_dir)
     return static_file(filepath, root=css_dir)
 
 
