b7c1cdd70bd284c8c2194a46a2059c27d48f0397
carlo-astro <carlo@ufrj.br>
2023-08-21 16:29:29 -0300

    Registra os três níveis e inicia o datasource (8/21a).

--- game/view/_core/pager.py
+++ game/view/_core/pager.py
@@ -9,143 +9,173 @@ Changelog
 .. versionadded::    23.08
         Create new project, package and module (17).
         Gerencia a mudança de nível no cliente (21).
+        Registra os três níveis e inicia o datasource (21a).
 
 |   **Open Source Notification:** This file is part of open source program **Alite**
 |   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
-|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
-|   `Labase <http://labase.selfip.org/>`_ - `NCE <http://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <https://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 """
-from unittest.mock import ANY, patch
+import uuid
+from unittest.mock import patch
 from collections import namedtuple
+from dataclasses import dataclass
+ELEM_ID = "opn eng dis clo exi mod name_ desc_".split()
 
-Level = namedtuple("Level", "title descript name m_dsc m_tit m_nam hpn hpd")
+Level = namedtuple("Level", "title descript name m_dsc m_tit m_nam hpn hpd img")
+# ObjectId = namedtuple("ObjectId", ELEM_ID)
+ObjectId = namedtuple("ObjectId", "opn eng dis clo exi mod name_ desc_")
 TEX0 = dict(descript="Create a new Python project - New Project - Click Here",
             name="Novo Projeto - Clique Aqui",
             title="Novo Projeto",
             m_dsc="Defina seu novo projeto",
             m_nam="Novo Projeto- Clique Aqui",
-            m_tit="Novo Projeto",
+            m_tit="Descreva seu Novo Projeto",
             hpn="O nome do projeto com uma única palavra minúscula e sem acentos.",
-            hpd="Um texto de uma linha descrevendo o projeto."
+            hpd="Um texto de uma linha descrevendo o projeto.",
+            img="../image/montroig.jpg"
             )
 TEX1 = dict(descript="Create a new Python packet - New Packet - Click Here",
             name="Novo Pacote - Clique Aqui",
             title="Novo Pacote",
             m_dsc="Defina seu novo pacote",
             m_nam="Novo Pacote - Clique Aqui",
-            m_tit="Novo Pacote",
+            m_tit="Descreva seu Novo Pacote",
             hpn="O nome do pacote com uma única palavra minúscula e sem acentos.",
-            hpd="Um texto de uma linha descrevendo o pacote."
+            hpd="Um texto de uma linha descrevendo o pacote.",
+            img="../image/garden.jpg"
             )
 TEX2 = dict(descript="Create a new Python Module - New Module - Click Here",
             name="Novo Módulo - Clique Aqui",
             title="Novo Módulo",
             m_dsc="Defina seu novo Módulo",
             m_nam="Novo Módulo - Clique Aqui",
-            m_tit="Novo Módulo",
+            m_tit="Descreva seu Novo Módulo",
             hpn="O nome do Módulo com uma única palavra minúscula e sem acentos.",
-            hpd="Um texto de uma linha descrevendo o Módulo."
+            hpd="Um texto de uma linha descrevendo o Módulo.",
+            img="../image/montroig.jpg"
             )
-LEVEL = dict(projeto=Level(**TEX0), pacote=Level(**TEX1), modulo=Level(**TEX2))
+LEVEL = dict(projeto=Level(**{s: str(t) for s, t in TEX0.items()}),
+             pacote=Level(**{s: str(t) for s, t in TEX1.items()}),
+             modulo=Level(**{s: str(t) for s, t in TEX2.items()}))
+OI = ObjectId(**{suf: str(suf if "_" in suf else f"_modal_{suf}") for suf in ELEM_ID})
+
+
+class DataSource:
+    def __init__(self):
+        @dataclass
+        class LevelData:
+            desc: str
+            name: str
+            title: str
+            pad: str
+
+        self.data_buffer = {}
+        self.level = LevelData
+
+    def save(self, desc, name, title, pad=None):
+        pad = pad or str(uuid.uuid4().fields[-1])[:9]
+        data = self.level(desc, name, title, pad)
+        import json
+        self.data_buffer[pad] = json.dumps(data)
+        return data
+
+    def load(self, pad):
+        import json
+        data = json.loads(self.data_buffer[pad])
+        data = self.level(**data)
+        return data
 
 class Action:
+
     def __init__(self, document, html):
         self.document = document
-        self.modal_id = [suf for suf in "mod opn eng dis clo exi pna pde".split()]
+        level_state = zip("projeto pacote modulo".split(), "pacote modulo projeto".split())
+        self.current_level = "projeto"
+        self.level = {lvl: nxt for lvl, nxt in level_state}
+        self.modal_id = [suf for suf in ELEM_ID]
         self.tags = html.DIV, html.FIGURE, html.A, html.IMG, html.SPAN, html.H4
-        # p, b, hd, sc, fm, fs, ip, lg, lb = self.tagr
-
         self.tagr = (html.P, html.BUTTON, html.HEADER, html.SECTION, html.FORM,
                      html.FIELDSET, html.INPUT, html.LEGEND, html.LABEL, html.FOOTER)
-        self.modal = document["_modal"]
-        self.modal_btn = document["_modal_go"]
-        self.modal_xxx = document["_close_top"]
-        self.modal_nop = document["_close_btn"]
-        self.modal_dis = document["_dismiss"]
-        self.modal_exe = document["_engage"]
-        self.panel_desc = document["_proj_desc"]
-        self.modal_desc = document["_projeto_desc"]
-        self.modal_btn.bind("click", self.open_modal)
-        self.modal_exe.bind("click", self.execute_modal)
-        self.modal_xxx.bind("click", self.close_modal)
-        self.modal_nop.bind("click", self.close_modal)
-        self.modal_dis.bind("click", self.close_modal)
-
-    def _init(self):
-        md, me, md, mc, mx, m5, m6, m7 = [f'_modal_{ix}' for ix in range(7)]
-
-        self.modal_id = [suf for suf in "mod eng dis clo exi pna pde".split()]
-        self.modal = [self.document[f"_modal_{ix}"] for ix in self.modal_id]
-        bnd = [self.open_modal, self.execute_modal] + [self.close_modal] * 3
-        [self.modal[bid].bind("click", handler) for bid, handler in zip(self.modal_id, bnd)]
+        self.modal = {}
+        # self.modal_id = [f"_modal_{suf}" for suf in "opn eng dis clo exi mod pna pde".split()]
+        self.bnd = [self.open_modal, self.execute_modal] + [self.close_modal] * 3
+    def build(self):
+        self.document["_panel"].html = ""
+        current_level_text = LEVEL[self.current_level]
+        _ = self.document["_panel"] <= self.create_card(current_level_text)
         self.document["_modal_mod"].remove()
-        _ = self.document.body <= self.create_modal()
+        _ = self.document.body <= self.create_modal(current_level_text)
+        self.modal = {inx: self.document[tg] for inx, tg in OI._asdict().items()}
+        [self.modal[bid].bind("click", handler) for bid, handler in zip(self.modal_id, self.bnd)]
 
     def open_modal(self, ev):
         ev.stopPropagation()
         ev.preventDefault()
         print("open")
-        self.modal.classList.add('is-active')
+        self.modal["mod"].classList.add('is-active')
 
     def close_modal(self, ev):
         ev.stopPropagation()
         ev.preventDefault()
-        self.modal.classList.remove('is-active')
+        self.modal["mod"].classList.remove('is-active')
 
     def execute_modal(self, ev):
         ev.stopPropagation()
         ev.preventDefault()
-        print(self.modal_desc.value)
-        self.panel_desc.html = self.modal_desc.value
-        self.modal.classList.remove('is-active')
+        # print(self.modal[""])
+        name, desc = self.modal["name_"].value, self.modal["desc_"].value,
+        self.modal["mod"].classList.remove('is-active')
+        img = LEVEL[self.current_level].img
+        inst = f"Entre no Projeto {name.capitalize()} -Clique Aqui"
+        texto = Level(f"Projeto {name.capitalize()}", desc, inst, *(["_NO_"]*5+[img]))
+        s = "opacity:0.7; position: absolute; min-width: 400%; min-height: 400%; top=-25%; left=0px;"
+        _ = self.document["_panel"] <= self.create_card(texto, s, f"_prj_{name}_")
 
-    def create_card(self):
+    def create_card(self, v, style=None, bind_id=None):
         d, f, a, i, s, h = self.tags
-        md, mo, me, md, mc, mx, m5, m6 = self.modal_id
-
-        image = "../image/montroig.jpg"
-        style = "opacity:0.5; filter:brightness(200%) blur(4px)"
-        descript = "Create a new Python project - New Project - Click Here"
-        name = "Novo Project - Clique Aqui"
-        title = "Novo Project"
+        o = bind_id or OI.opn
+        img, title, name, descript = v.img, v.title, v.name, v.descript
+        style = style or "opacity:0.5; filter:brightness(200%) blur(1px)"
         card = d(
-            d(f(a(i(src=image, style=style)), Class="image is-4by3"), Class="card-image") +
+            d(d(f(a(i(src=img, style=style)), Class="image is-4by3 is-clipped"), Class="card-image") +
             d(s(title), Class="card-content is-overlay is-size-1 has-text-weight-bold has-text-black") +
             d(h(name, Class="title is-4"), Class="card-content-header") +
             s(descript)  # , id=m1)
-            , Class="card", id=mo)
+            , Class="card", style="height:100%; overflow:hidden", id=o), Class="column is-4")
         return card
 
-    def create_modal(self, level="projeto"):
-        def field(_name="_name", ph="nomedoprojeto", hlp=""):
+    def create_modal(self, tx):
+        def field(_name=ELEM_ID[-2], ph="nomedoprojeto", hlp=""):
             return d(lb(name, For=_name, Class="label") +
                      d(ip(id=_name, name=_name, type="text", placeholder=ph, Class="input") +
                        p(hlp, Class="help"), Class="control"))
 
         d, f, a, i, s, h = self.tags
         p, b, hd, sc, fm, fs, ip, lg, lb, ft = self.tagr
-        tx = LEVEL[level]
+        mod, opn, eng, dis, clo, exi = OI.mod, OI.opn, OI.eng, OI.dis, OI.clo, OI.exi
+        # tx = LEVEL[level]
         # md, me, md, mc, mx, m5, m6, m7 = [f'_modal_{_ix}' for _ix in range(7)]
-        md, mo, me, md, mc, mx, m5, m6 = self.modal_id
+        # md, mo, me, md, mc, mx, m5 = self.modal_id
         # descript = "Defina seu novo projeto"
         # name = "Novo Project - Clique Aqui"
         # title = "Novo Projeto"
         # hpr = "O nome do projeto com uma única palavra minúscula e sem acentos."
         # hpd = "Um texto de uma linha descrevendo o projeto."
-        descript = tx.descript
+        descript = tx.m_tit
         name = tx.name
         title = tx.title
         hpn, hpd = tx.hpn, tx.hpd
         ii, ig = "button is-info", "button is-danger"
         card = d(
             d(hd(p(descript, Class="modal-card-title") +
-                 b(Class="delete", id=mx, ariaLabel="close"), Class="modal-card-head"), Class="modal-card") +
+                 b(Class="delete", id=exi, ariaLabel="close"), Class="modal-card-head") +
             sc(d(fm(
-                fs(lg(title) + field(hlp=hpn) + field(_name="_desc", hlp=hpd, ph="descreve o projeto")),
+                fs(lg(title) + field(hlp=hpn) + field(_name=ELEM_ID[-1], hlp=hpd, ph="descreve o projeto")),
                 Class="form-horizontal"), Class="content"), Class="modal-card-body") +
-            ft(b("Enviar", Class=ii, id=me) + b("Cancelar", Class=ii, id=md) +
-               b("Fechar", Class=ig, id=mc), Class="modal-card-foot"),
-            Class="modal", id=md)
+            ft(b("Enviar", Class=ii, id=eng) + b("Cancelar", Class=ii, id=dis) +
+               b("Fechar", Class=ig, id=clo), Class="modal-card-foot"), Class="modal-card"),
+            Class="modal", id=mod)
         return card
 
 
@@ -160,8 +190,9 @@ if __name__ == '__main__':
         _act = Action(dic, htm)
         assert my_btn.assert_any_call()
         return _act
-    ix = ("_modal", "_modal_go", "_close_top", "_close_btn", "_dismiss",
+    ixx = ("_modal", "_modal_go", "_close_top", "_close_btn", "_dismiss",
           "_engage", "_proj_desc", "_projeto_desc")
+    ix = [f"_modal_{suf}" for suf in "mod opn eng dis clo exi pna pde".split()]
     _html = MagicMock
     dc = {idx: Mock(name=f"dcm_{idx}") for idx in ix}
     mk = {idx:binder(idx, dc[idx]) for idx in ix}
@@ -176,13 +207,16 @@ if __name__ == '__main__':
     _html.FIELDSET, _html.INPUT, _html.LEGEND, _html.LABEL, _html.FOOTER = [myMagic] *5
 
     act = Action(dc, _html)
-    m = act.create_modal()
-    cd = act.create_card()
+    m = act.create_modal(LEVEL["projeto"])
+    cd = act.create_card(LEVEL["projeto"])
     # _html.DIV.DIV.assert_any_call()
     print(_add.mock_calls)
     print(_html.DIV.mock_calls)
     print(_html.BUTTON.mock_calls)
-    assert act.modal_btn == dc[ix[1]], (act.modal_btn, dc[ix[1]])
+    print(" ".join(ELEM_ID))
+    print(OI._asdict())
+    print()
+    #assert act.modal_btn == dc[ix[1]], (act.modal_btn, dc[ix[1]])
     #mk[ix[1]].bind.assert_called_with()
 '''
 <div class="modal" id="_modal">
