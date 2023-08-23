0228d54cb0fee944f97ce5fa8ae4720f1c48ea8d
carlo-astro <carlo@ufrj.br>
2023-08-22 21:27:02 -0300

    Refactor for small data entry, new card and modal (22).

--- game/test/test_pager.py
+++ game/test/test_pager.py
@@ -23,31 +23,54 @@ from view._core.pager import Action, ELEM_ID, LEVEL
 
 class TestCorePager(unittest.TestCase):
     def setUp(self) -> None:
-        class AddMock(Mock):
+        class AddMock(MagicMock, dict):
+            body = MagicMock(name="doc_body")
             def __add__(self, other):
                 return MagicMock(name="doc_add", _value=other)
+            def __getitem__(self, item):
+
+                return MagicMock(name="doc_item", _value=item)
+            def __le__(self, other):
+                return AddMock(name="doc_inc", _value=other)
+        magic_doc = AddMock()
+        magic_doc.body = AddMock(name="doc_body")
         def binder(_, mck):
             mck.bind = Mock()
             return mck.bind
+        # @patch.dict(magic_doc, {"_panel": AddMock(name="doc_item"), "_modal_mod": AddMock(name="doc_mod")})
+        def doc_mock():
+            # magic_doc.body = AddMock(name="doc_body")
+            return Action(magic_doc, _html)
         def html_mock(mocker)-> (MagicMock, dict):
             html_dc = {f: AddMock(name=f"html_{f}") for f in mocker}
             html_ls = [html_dc[key] for key in mocker]
             mh = MagicMock(name="html_mock", return_value = html_ls)
             return mh, html_dc
+
         ix = [f"_modal_{suf}" for suf in ELEM_ID]
         _html = MagicMock
         dc = {idx: Mock(name=f"dcm_{idx}") for idx in ix}
         _ = {idx:binder(idx, dc[idx]) for idx in ix}
-        magic_doc = MagicMock
+        # magic_doc = AddMock
+        # magic_doc["_panel"].html = MagicMock(name="doc_panel")
         t_one, t_two = "d, f, a, i, s, h".split(", "), "p, b, hd, sc, fm, fs, ip, lg, lb, ft".split(", ")
-        magic_doc.__add__ = self._add = MagicMock(name="doc_add")
+        # magic_doc.__add__ = self._add = MagicMock(name="doc_add")
+        # magic_doc.__getitem__.return_value = self._get = MagicMock(name="doc_get")
         _html.DIV, _html.FIGURE, _html.A, _html.IMG, _html.SPAN, _html.H4 = html_mock(t_one)[1].values()
         (_html.P, _html.BUTTON, _html.HEADER, _html.SECTION, _html.FORM,
         _html.FIELDSET, _html.INPUT, _html.LEGEND, _html.LABEL, _html.FOOTER) = html_mock(t_two)[1].values()
         self.am =AddMock
-        self.act = Action(magic_doc, _html)
+        # magic_doc = {"_panel": AddMock(name="doc_item"), "_modal_mod": AddMock(name="doc_mod")}
+        # with patch.dict(magic_doc, {"_panel": AddMock(name="doc_item")}):
+        #     self.act = Action(magic_doc, _html)
+        self.act = doc_mock()
         self.act.h_one.get_one, self.html_one = html_mock(self.act.h_one.tags_one)
         self.act.h_one.get_two, self.html_two = html_mock(self.act.h_one.tags_two)
+    def test_build(self):
+        self.act.document["_panel"].__le__ = self.am(name="doc_panel")
+        self.act.document.body.__le__ = self.am(name="doc_body_le")
+        _ = self.act.build()
+        _ = self.html_one["d"]
     def test_create_card(self):
         _ = self.act.create_card(LEVEL["projeto"])
         d = self.html_one["d"]
@@ -57,10 +80,9 @@ class TestCorePager(unittest.TestCase):
         self.html_one["i"].assert_called_with(**img)
         moc_calls = d.mock_calls
 
-        self.assertEqual(5, len(moc_calls))  # add assertion here
+        self.assertEqual(8, len(moc_calls))  # add assertion here
         d.assert_called_with(ANY, Class=ANY)
-    def test_create_modal(self):
-        _ = self.act.create_modal(LEVEL["projeto"])
+    def _create_modal(self):
         d = self.html_two["p"]
         b = self.html_two["b"]
         b.assert_called_with(ANY, Class='button is-danger', id='_modal_clo')
@@ -70,8 +92,17 @@ class TestCorePager(unittest.TestCase):
         self.html_two["fs"].assert_called_once()
         moc_calls = b.mock_calls
         # print(moc_calls)
-        self.assertEqual(4, len(moc_calls))  # add assertion here
+        self.assertEqual(6, len(moc_calls))  # add assertion here
         d.assert_called_with(ANY, Class=ANY)
+    def test_create_modal(self):
+        _ = self.act.create_modal(LEVEL["projeto"], LEVEL["projeto_modal"])
+        self._create_modal()
+    def test_create_modal_packet(self):
+        _ = self.act.create_modal(LEVEL["pacote"], LEVEL["pacote_modal"])
+        self._create_modal()
+    def test_create_modal_module(self):
+        _ = self.act.create_modal(LEVEL["modulo"], LEVEL["modulo_modal"])
+        self._create_modal()
 
 
 if __name__ == '__main__':
--- game/view/_core/pager.py
+++ game/view/_core/pager.py
@@ -7,9 +7,10 @@
 Changelog
 ---------
 .. versionadded::    23.08
-        Create new project, package and module (17).
-        Gerencia a mudança de nível no cliente (21).
+        Refactor for small data entry, new card and modal (22).
         Registra os três níveis e inicia o datasource (21a).
+        Gerencia a mudança de nível no cliente (21).
+        Create new project, package and module (17).
 
 |   **Open Source Notification:** This file is part of open source program **Alite**
 |   **Copyright © 2023  Carlo Oliveira** <carlo@nce.ufrj.br>,
@@ -17,80 +18,94 @@ Changelog
 |   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 """
 import uuid
-from unittest.mock import patch
 from collections import namedtuple
-from dataclasses import dataclass, asdict
 
 ELEM_ID = "opn eng dis clo exi mod name_ desc_".split()
-
-Level = namedtuple("Level", "descript name title m_dsc m_tit m_nam h_nam h_dsc h_tit")
+Level = namedtuple("Level", "descript name title extra")
 ObjectId = namedtuple("ObjectId", "opn eng dis clo exi mod name_ desc_")
-TEX0 = dict(descript="Create a new Python project - New Project - Click Here",
-            name="Novo Projeto - Clique Aqui",
-            title="Novo Projeto",
-            m_dsc="Defina seu novo projeto",
-            m_nam="Novo Projeto- Clique Aqui",
-            m_tit="Descreva seu Novo Projeto",
-            h_nam="O nome do projeto com uma única palavra minúscula e sem acentos.",
-            h_dsc="Um texto de uma linha descrevendo o projeto.",
-            h_tit="../image/montroig.jpg"
-            )
-TEX1 = dict(descript="Create a new Python packet - New Packet - Click Here",
-            name="Novo Pacote - Clique Aqui",
-            title="Novo Pacote",
-            m_dsc="Defina seu novo pacote",
-            m_nam="Novo Pacote - Clique Aqui",
-            m_tit="Descreva seu Novo Pacote",
-            h_nam="O nome do pacote com uma única palavra minúscula e sem acentos.",
-            h_dsc="Um texto de uma linha descrevendo o pacote.",
-            h_tit="../image/garden.jpg"
-            )
-TEX2 = dict(descript="Create a new Python Module - New Module - Click Here",
-            name="Novo Módulo - Clique Aqui",
-            title="Novo Módulo",
-            m_dsc="Defina seu novo Módulo",
-            m_nam="Novo Módulo - Clique Aqui",
-            m_tit="Descreva seu Novo Módulo",
-            h_nam="O nome do Módulo com uma única palavra minúscula e sem acentos.",
-            h_dsc="Um texto de uma linha descrevendo o Módulo.",
-            h_tit="../image/montroig.jpg"
-            )
-LEVEL = dict(projeto=Level(**{s: str(t) for s, t in TEX0.items()}),
-             pacote=Level(**{s: str(t) for s, t in TEX1.items()}),
-             modulo=Level(**{s: str(t) for s, t in TEX2.items()}))
 OI = ObjectId(**{suf: str(suf if "_" in suf else f"_modal_{suf}") for suf in ELEM_ID})
+
+class DATA:
+    TEX0 = dict(descript="Create a new Python project - New Project - Click Here",
+                name="Novo Projeto - Clique Aqui",
+                title="Novo Projeto",
+                extra="../image/montroig.jpg"
+                )
+    MOD0 = dict(descript="Defina seu novo pacote",
+                name="Novo Pacote - Clique Aqui",
+                title="Descreva seu Novo Pacote",
+                extra=("O nome do pacote com uma única palavra minúscula e sem acentos.",
+                       "Um texto de uma linha descrevendo o pacote."),
+                )
+    TEX1 = dict(descript="Create a new Python packet - New Packet - Click Here",
+                name="Novo Pacote - Clique Aqui",
+                title="Novo Pacote",
+                extra="../image/garden.jpg"
+                )
+    MOD1 = dict(descript="Defina seu novo pacote",
+                name="Novo Pacote - Clique Aqui",
+                title="Descreva seu Novo Pacote",
+                extra=("O nome do pacote com uma única palavra minúscula e sem acentos.",
+                       "Um texto de uma linha descrevendo o pacote."),
+                )
+    TEX2 = dict(descript="Create a new Python Module - New Module - Click Here",
+                name="Novo Módulo - Clique Aqui",
+                title="Novo Módulo",
+                extra="../image/montroig.jpg"
+                )
+    MOD2 = dict(descript="Defina seu novo Módulo",
+                name="Novo Módulo - Clique Aqui",
+                title="Descreva seu Novo Módulo",
+                extra=("O nome do Módulo com uma única palavra minúscula e sem acentos.",
+                       "Um texto de uma linha descrevendo o Módulo."),
+                )
+LEVEL = dict(projeto=Level(**{s: str(t) for s, t in DATA.TEX0.items()}),
+             projeto_modal=Level(**{s: str(t) for s, t in DATA.MOD0.items()}),
+             pacote=Level(**{s: str(t) for s, t in DATA.TEX1.items()}),
+             pacote_modal=Level(**{s: str(t) for s, t in DATA.MOD1.items()}),
+             modulo=Level(**{s: str(t) for s, t in DATA.TEX2.items()}),
+             modulo_modal=Level(**{s: str(t) for s, t in DATA.MOD2.items()}))
 
 
 class DataSource:
     def __init__(self):
-        @dataclass
+        # @dataclass
         class LevelData:
             descript: str
             name: str
             title: str
-            m_dsc: str
-            m_tit: str
-            m_nam: str
-            h_nam: str
-            h_dsc: str
-            h_tit: str
+            extra: str
             pad: str
 
+            def __init__(self, descript: str, name: str, title: str, extra: str, pad: str):
+                self.descript, self.name, self.title, self.extra =  descript, name, title, extra
+                self.pad = pad
+            def asdict(self):
+                return {k: getattr(self, k) for k in "descript, name, title, extra".split(", ")}
+
             def as_dict(self):
-                    return {k: str(v) for k, v in asdict(self).items()}
-        @dataclass
+                return {k: str(v) for k, v in self.asdict().items()}
+
+        # @dataclass
         class LevelContent:
             pid: str
             pad: list
 
+            def __init__(self, pid, pad):
+                self.pid, self.pad = pid, pad
+            def asdict(self):
+                return {k: getattr(self, k) for k in "pid pad".split()}
             def as_dict(self):
-                    return {k: str(v) for k, v in asdict(self).items()}
+                return {k: str(v) for k, v in self.asdict().items()}
+
             def upsert(self, _pad):
-                data = _pad if isinstance(_pad,list) else [_pad]
+                data = _pad if isinstance(_pad, list) else [_pad]
                 self.pad = self.pad + data if self.pad else data
+
         self.data_buffer = {}
         self.level = LevelData
         self.content = LevelContent
+
 
     def save_cnt(self, pid, pad):
         # print("save_cnt(self, pid, pad)", pid, pad, self.data_buffer[pid])
@@ -101,26 +116,29 @@ class DataSource:
         else:
             self.data_buffer[pid] = self.content(pid, data)
 
+
     def load_cnt(self, pid):
         data = self.data_buffer[pid].pad
-        print("load_cnt",pid, data)
+        print("load_cnt", pid, data)
         data = [self.data_buffer[pd] for pd in data if pd in self.data_buffer]
 
         return data
 
-    def save(self, descript, name, title, m_dsc,  m_tit,  m_nam, h_nam,  h_dsc,  h_tit,  pad=None):
+
+    def save(self, descript, name, title, extra, pad=None):
         import json
         pad = pad or str(uuid.uuid4().fields[-1])[:9]
-        data = [""]*6+[pad]
-        data = self.level(descript, name, title, m_dsc,  m_tit,  m_nam, h_nam,  h_dsc,  h_tit, pad).as_dict()
+        data = self.level(descript, name, title, extra, pad).as_dict()
         self.data_buffer[pad] = json.dumps(data)
         return data
+
 
     def load(self, pad):
         import json
         data = json.loads(self.data_buffer[pad])
         data = self.level(**data)
         return data.as_dict()
+
 
 class Html:
     def __init__(self, html):
@@ -131,10 +149,13 @@ class Html:
         self.tags_two = "p, b, hd, sc, fm, fs, ip, lg, lb, ft".split(", ")
         [setattr(self, name, value) for name, value in zip(self.tags_one, self.tags)]
         [setattr(self, name, value) for name, value in zip(self.tags_two, self.tagr)]
+
     def get_one(self):
         return [getattr(self, name) for name in self.tags_one]
+
     def get_two(self):
         return [getattr(self, name) for name in self.tags_two]
+
 
 class Action:
 
@@ -144,18 +165,16 @@ class Action:
         self.current_level = "projeto"
         self.level = {lvl: nxt for lvl, nxt in level_state}
         self.modal_id = [suf for suf in ELEM_ID]
-        # self.tags = html.DIV, html.FIGURE, html.A, html.IMG, html.SPAN, html.H4
-        # self.tagr = (html.P, html.BUTTON, html.HEADER, html.SECTION, html.FORM,
-        #              html.FIELDSET, html.INPUT, html.LEGEND, html.LABEL, html.FOOTER)
         self.h_one = Html(html)
         self.modal = {}
         self.data = DataSource()
         self.bnd = [self.open_modal, self.execute_modal] + [self.close_modal] * 3
         self.levels = self.populate()
+
     def populate(self):
-        p = self.data.save(**LEVEL["projeto"]._asdict(), pad="__projeto@__")
-        k = self.data.save(**LEVEL["pacote"]._asdict(), pad="__pacote@__")
-        md = self.data.save(**LEVEL["modulo"]._asdict(), pad="__modulo@__")
+        _ = self.data.save(**LEVEL["projeto"]._asdict(), pad="__projeto@__")
+        _ = self.data.save(**LEVEL["pacote"]._asdict(), pad="__pacote@__")
+        _ = self.data.save(**LEVEL["modulo"]._asdict(), pad="__modulo@__")
         lvs = dict(__projeto__="__projeto@__", __pacote__="__pacote@__", __modulo__="__modulo@__")
         [self.data.save_cnt(pid, pad) for pid, pad in lvs.items()]
         return lvs
@@ -163,9 +182,10 @@ class Action:
     def build(self):
         self.document["_panel"].html = ""
         current_level_text = LEVEL[self.current_level]
+        current_level_modal = LEVEL[f"{self.current_level}_modal"]
         _ = self.document["_panel"] <= self.create_card(current_level_text)
         self.document["_modal_mod"].remove()
-        _ = self.document.body <= self.create_modal(current_level_text)
+        _ = self.document.body <= self.create_modal(current_level_text, current_level_modal)
         self.modal = {inx: self.document[tg] for inx, tg in OI._asdict().items()}
         [self.modal[bid].bind("click", handler) for bid, handler in zip(self.modal_id, self.bnd)]
 
@@ -183,29 +203,28 @@ class Action:
     def execute_modal(self, ev):
         ev.stopPropagation()
         ev.preventDefault()
-        # print(self.modal[""])
         name, desc = self.modal["name_"].value, self.modal["desc_"].value,
         self.modal["mod"].classList.remove('is-active')
-        h_tit = LEVEL[self.current_level].h_tit
+        extra = LEVEL[self.current_level].extra
         inst = f"Entre no Projeto {name.capitalize()} -Clique Aqui"
-        texto = Level(f"Projeto {name.capitalize()}", desc, inst, *(["_NO_"]*5+[h_tit]))
+        texto = Level(desc, inst, f"Projeto {name.capitalize()}", extra)
         s = "opacity:0.7; position: absolute; min-width: 400%; min-height: 400%; top=-25%; left=0px;"
         _ = self.document["_panel"] <= self.create_card(texto, s, f"_prj_{name}_")
 
     def create_card(self, v, style=None, bind_id=None):
         d, f, a, i, s, h = self.h_one.get_one()
         o = bind_id or OI.opn
-        h_tit, title, name, descript = v.h_tit, v.title, v.name, v.descript
+        h_tit, title, name, descript = v.extra, v.title, v.name, v.descript
         style = style or "opacity:0.5; filter:brightness(200%) blur(1px)"
         card = d(
             d(d(f(a(i(src=h_tit, style=style)), Class="image is-4by3 is-clipped"), Class="card-image") +
-            d(s(title), Class="card-content is-overlay is-size-1 has-text-weight-bold has-text-black") +
-            d(h(name, Class="title is-4"), Class="card-content-header") +
-            s(descript)  # , id=m1)
-            , Class="card", style="height:100%; overflow:hidden", id=o), Class="column is-4")
+              d(s(title), Class="card-content is-overlay is-size-1 has-text-weight-bold has-text-black") +
+              d(h(name, Class="title is-4"), Class="card-content-header") +
+              s(descript)  # , id=m1)
+              , Class="card", style="height:100%; overflow:hidden", id=o), Class="column is-4")
         return card
 
-    def create_modal(self, tx):
+    def create_modal(self, tx, tm):
         def field(_name=ELEM_ID[-2], ph="nomedoprojeto", hlp=""):
             return d(lb(name, For=_name, Class="label") +
                      d(ip(id=_name, name=_name, type="text", placeholder=ph, Class="input") +
@@ -214,130 +233,22 @@ class Action:
         d, f, a, i, s, h = self.h_one.get_one()
         p, b, hd, sc, fm, fs, ip, lg, lb, ft = self.h_one.get_two()
         mod, opn, eng, dis, clo, exi = OI.mod, OI.opn, OI.eng, OI.dis, OI.clo, OI.exi
-        descript = tx.m_tit
+        descript = tm.title
         name = tx.name
         title = tx.title
-        h_nam, h_dsc = tx.h_nam, tx.h_dsc
+        h_nam, h_dsc = tm.name, tm.descript
         ii, ig = "button is-info", "button is-danger"
         card = d(
             d(hd(p(descript, Class="modal-card-title") +
                  b(Class="delete", id=exi, ariaLabel="close"), Class="modal-card-head") +
-            sc(d(fm(
-                fs(lg(title) + field(hlp=h_nam) + field(_name=ELEM_ID[-1], hlp=h_dsc, ph="descreve o projeto")),
-                Class="form-horizontal"), Class="content"), Class="modal-card-body") +
-            ft(b("Enviar", Class=ii, id=eng) + b("Cancelar", Class=ii, id=dis) +
-               b("Fechar", Class=ig, id=clo), Class="modal-card-foot"), Class="modal-card"),
+              sc(d(fm(
+                  fs(lg(title) + field(hlp=h_nam) + field(_name=ELEM_ID[-1], hlp=h_dsc, ph="descreve o projeto")),
+                  Class="form-horizontal"), Class="content"), Class="modal-card-body") +
+              ft(b("Enviar", Class=ii, id=eng) + b("Cancelar", Class=ii, id=dis) +
+                 b("Fechar", Class=ig, id=clo), Class="modal-card-foot"), Class="modal-card"),
             Class="modal", id=mod)
         return card
 
 
 if __name__ == '__main__':
-    from unittest.mock import MagicMock, Mock
-    def binder(ii,mck):
-        mck.bind = Mock()
-        return mck.bind
-    my_btn = Mock()
-    @patch.object(Action,"modal_btn", my_btn.btn)
-    def action(dic, htm):
-        _act = Action(dic, htm)
-        assert my_btn.assert_any_call()
-        return _act
-    ixx = ("_modal", "_modal_go", "_close_top", "_close_btn", "_dismiss",
-          "_engage", "_proj_desc", "_projeto_desc")
-    ix = [f"_modal_{suf}" for suf in "mod opn eng dis clo exi pna pde".split()]
-    _html = MagicMock
-    dc = {idx: Mock(name=f"dcm_{idx}") for idx in ix}
-    mk = {idx:binder(idx, dc[idx]) for idx in ix}
-    myMagic = MagicMock
-    test_mock = MagicMock
-    myMagic.__add__ = _add = MagicMock()
-    # myMagic.__new__ = test_mock
-    _html.DIV, _html.FIGURE, _html.A, _html.IMG, _html.SPAN, _html.H4 = [myMagic] *6
-    # p, b, hd, sc, fm, fs, ip, lg, lb = self.tagr
-
-    _html.P, _html.BUTTON, _html.HEADER, _html.SECTION, _html.FORM = [myMagic] *5
-    _html.FIELDSET, _html.INPUT, _html.LEGEND, _html.LABEL, _html.FOOTER = [myMagic] *5
-
-    act = Action(dc, _html)
-    m = act.create_modal(LEVEL["projeto"])
-    cd = act.create_card(LEVEL["projeto"])
-    # _html.DIV.DIV.assert_any_call()
-    print(_add.mock_calls)
-    ds = DataSource()
-    ind = ds.save("adesc", "aname", "atitle", *([""]*6))
-    print(ind)
-    print(ds.load(ind["pad"]))
-    print(act.data.load_cnt("__projeto__"))
-    print(act.data.data_buffer)
-'''
-<div class="modal" id="_modal">
-    <div class="modal-background"></div>
-    <div class="modal-card">
-        <header class="modal-card-head">
-            <p class="modal-card-title">Defina seu novo projeto</p>
-            <button class="delete" id="_close_top" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-            <div class="content">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <!-- Form Name -->
-                        <legend>Novo Projeto</legend>
-                        <!-- Text input-->
-                        <div class="field">
-                            <label class="label" for="_projeto">Nome do Projeto</label>
-                            <div class="control">
-                                <input id="_projeto" name="_projeto" type="text" placeholder="nomedoprojeto"
-                                       class="input " required="">
-                                <p class="help">O nome do projeto com uma única palavra minúscula e sem acentos.</p>
-                            </div>
-                        </div>
-                        <!-- Text input-->
-                        <div class="field">
-                            <label class="label" for="_projeto_desc">Descrição do Projeto</label>
-                            <div class="control">
-                                <input id="_projeto_desc" name="_projeto_desc" type="text" placeholder="descreveprojeto"
-                                       class="input " required="">
-                                <p class="help">Um texto de uma linha descrevendo o projeto.</p>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-        </section>
-        <footer class="modal-card-foot">
-            <button class="button is-info" id="_engage">Yes</button>
-            <button class="button is-info" id="_dismiss">No</button>
-            <button class="button is-danger" id="_close_btn">Close</button>
-        </footer>
-    </div>
-</div>
-
-'''
-"""
-         <div class="card" id="_modal_go">
-                    <!-- image for post -->
-                    <div class="card-image">
-                        <figure class="image is-4by3">
-                            <a href="">
-                                <img src="../image/novo_projeto.png"
-                                     style="opacity:0.5; filter:brightness(200%) blur(4px)"  alt="Novo Projeto">
-                            </a>
-                        </figure>
-                    </div>
-                    <!-- end of image for post -->
-                       <div class="card-content is-overlay is-size-1 has-text-weight-bold has-text-black">
-                            <span>Novo Projeto</span>
-                            <!-- some content to be placed over the image -->
-                        </div>
-
-                    <!-- post header -->
-                    <div class="card-content-header">
-                        <h4 class="title is-4"><a href="">Novo Projeto - Clique Aqui</a></h4>
-                    </div>
-                    <!-- end of post header -->
-                    <span id="_proj_desc">Create a new Python project - New Project - Click Here</span>
-                    <!-- post content -->
-                </div>
-       
-"""
+    pass
